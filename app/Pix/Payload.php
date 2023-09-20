ff0cb2568425e56cc6cbdd12b24be8000549d247
etmjr03 <etmjr03@gmail.com>
2023-09-20 00:14:35 -0300

    First commit

---
+++ app/Pix/Payload.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Pix;
+
+class Payload {
+    /**
+     * IDs do Payload do Pix
+    * @var string
+    */
+    const ID_PAYLOAD_FORMAT_INDICATOR = '00';
+    const ID_MERCHANT_ACCOUNT_INFORMATION = '26';
+    const ID_MERCHANT_ACCOUNT_INFORMATION_GUI = '00';
+    const ID_MERCHANT_ACCOUNT_INFORMATION_KEY = '01';
+    const ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION = '02';
+    const ID_MERCHANT_CATEGORY_CODE = '52';
+    const ID_TRANSACTION_CURRENCY = '53';
+    const ID_TRANSACTION_AMOUNT = '54';
+    const ID_COUNTRY_CODE = '58';
+    const ID_MERCHANT_NAME = '59';
+    const ID_MERCHANT_CITY = '60';
+    const ID_ADDITIONAL_DATA_FIELD_TEMPLATE = '62';
+    const ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID = '05';
+    const ID_CRC16 = '63';
+
+    // Chave do pix (string)  
+    private $chavePix;
+
+    // Descrição do pagamento (string)  
+    private $descricaoPagamento;
+
+    // Nome do titular da conta (string)
+    private $nomeTitular;
+
+    // Nome da cidade do titular da conta (string)
+    private $cidadeTitular;
+
+    // Id da transação do pix (string)
+    private $idTransacao;
+
+    // Valor da transção (string)
+    private $valorTransacao;
+
+}
