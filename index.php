2041f60deef2ef5c5b8c5441c2b845758d8d68dc
etmjr03 <etmjr03@gmail.com>
2023-09-20 00:38:02 -0300

    Instanciando a classe Payload

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
+
+use \App\Pix\Payload;
+
+// instancia da classe Payload com as informações do titular do pix
+$obPayload = (new Payload)->setChavePix('12345678900')
+                          ->setDescricaoPagamento('Pagamento enviado, valeu!')
+                          ->setNomeTitular('Juninho')
+                          ->setCidadeTitular('POCO FUNDO')
+                          ->setIdTransacao('Juninho1234')
+                          ->setValorTransacao(1.00);
